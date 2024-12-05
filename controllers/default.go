f1b1d123e4b69bc04c35d4e83cbb142502259ceb
gexin950228 <861439031@qq.com>
2024-12-05 10:49:22 +0800

    first get api

--- controllers/default.go
+++ controllers/default.go
@@ -8,8 +8,18 @@ type MainController struct {
 	beego.Controller
 }
 
+type GreetController struct {
+	beego.Controller
+}
+
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "gexin17812126257@163.com"
 	c.TplName = "index.tpl"
+}
+
+func (g *GreetController) Get() {
+	g.Data["Website"] = "gexin.me"
+	g.Data["Email"] = "861439031@qq.com"
+	g.TplName = "greet.tpl"
 }
