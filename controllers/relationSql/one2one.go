03b788eb4c8245c450fc9183d9baa181df0ef719
gexin950228 <861439031@qq.com>
2025-01-15 11:04:57 +0800

    1对1增删改查

--- controllers/relationSql/one2one.go
+++ controllers/relationSql/one2one.go
@@ -1,8 +1,6 @@
 package relationSql
 
 import (
-	"beeDemo/models"
-	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 )
@@ -11,42 +9,77 @@ type OneToOneController struct {
 	beego.Controller
 }
 
-type rep struct {
-	Code string
-	Msg  []string
-}
-
 func (c *OneToOneController) Get() {
 	orm.Debug = true
-	person := models.Person{
-		Id:           2,
-		Name:         "葛香",
-		IdCardNumber: "43038119871013361X",
-	}
 	o := orm.NewOrm()
-	//o.Insert(&person)
-	personProfile := models.PersonProfile{
-		Id:                  2,
-		Age:                 37,
-		Address:             "湖南省湘潭市",
-		Hobbies:             "看剧",
-		Email:               "email@qq.com",
-		EducationExperience: "高中",
-		Person:              &person,
-		Skills:              "美甲",
-	}
-	//o.Insert(&personProfile)
-	// 原生sql
-	ret := o.Raw("INSERT INTO person(id, name, id_card_number) VALUES(?, ?, ?)", person.Id, person.Name, person.IdCardNumber)
-	_, err := ret.Exec()
+
+	//person := models.Person{
+	//	Name:         "葛禄辉",
+	//	IdCardNumber: "43038119871013361X",
+	//}
+	//
+	//_, err := o.Insert(&person)
+	//if err != nil {
+	//	return
+	//}
+	//
+	//personProfile := models.PersonProfile{
+	//	Age:                 57,
+	//	Address:             "湖南省湘潭市",
+	//	Hobbies:             "看剧",
+	//	Email:               "geluhui@qq.com",
+	//	EducationExperience: "初中",
+	//	Person:              &person,
+	//	Skills:              "没有爱好",
+	//}
+	//_, err = o.Insert(&personProfile)
+	//if err != nil {
+	//	return
+	//}
+	//
+	//// 原生sql
+	//ret := o.Raw("INSERT INTO person( name, id_card_number) VALUES(?, ?)", person.Name, person.IdCardNumber)
+	//exec, err := ret.Exec()
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	//var insertId int64
+	//insertId, err = exec.LastInsertId()
+	//r2 := o.Raw("INSERT INTO person_profile(gender, age, address, hobbies, email, education_experience, person_id, skills)"+
+	//	" VALUES(?, ?, ?, ?, ?, ?, ?, ?)", personProfile.Gender, personProfile.Age, personProfile.Address, personProfile.Hobbies, personProfile.Email, personProfile.EducationExperience, insertId, personProfile.Skills)
+	//exec, err = r2.Exec()
+	//if err != nil {
+	//	return
+	//}
+	//id, _ := exec.LastInsertId()
+	//fmt.Println(id)
+
+	// 一对一修改
+	//
+	//update, err := qs.Filter("person_id__exact", 3).Update(orm.Params{"age": 57, "hobbies": "看小说"})
+	//if err != nil {
+	//	return
+	//} else {
+	//	fmt.Println(update)
+	//}
+
+	// 一对一删除orm
+	//i, err := qs.Filter("id__exact", 3).Delete()
+	//if err != nil {
+	//	return
+	//} else {
+	//	utils.LogToFile("INFO", fmt.Sprintf("id为%d的用户已删除", i))
+	//}
+	//  一对一删除原生sql
+	//r := o.Raw("delete from person_profile where id=?", 8)
+	//execId, err := r.Exec()
+	//if err != nil {
+	//	return
+	//}
+	r1 := o.Raw("DELETE FROM person where id=?", 8)
+	_, err := r1.Exec()
 	if err != nil {
-		fmt.Println(err.Error())
-	}
-	r2 := o.Raw("INSERT INTO person_profile(id, gender, age, address, hobbies, email, education_experience, person_id, skills) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-		person.Id, personProfile.Gender, personProfile.Age, personProfile.Address, personProfile.Hobbies, personProfile.Email, personProfile.EducationExperience, &person.Id, personProfile.Skills)
-	_, err = r2.Exec()
-	if err != nil {
-		fmt.Println(err.Error())
+		return
 	}
 	c.TplName = "relationSql/oneToOne.html"
 }
