5a5d7a2532fe194dc7be96d266c694d49e149865
gexin950228 <861439031@qq.com>
2025-01-24 16:03:34 +0800

    多对多修改

--- controllers/relationSql/addTag.go
+++ controllers/relationSql/addTag.go
@@ -47,24 +47,23 @@ func (r *ReportAddTagController) Get() {
 	}
 	_, err = o.LoadRelated(&report, "Labels")
 	if err != nil {
-		utils.LogToFile("ERROR", fmt.Sprintf("查询report标签信息报错，错误信息为: %s", err.Error()))
+		utils.LogToFile("ERROR", fmt.Sprintf("关联查询report标签信息报错，错误信息为: %s", err.Error()))
 	}
-	var allLabels []models.Labels
-	var existLabels []models.Labels
-	for i := range report.Labels {
-		var label models.Labels
-		o.QueryTable("labels").Filter("id", report.Labels[i].Id).One(&label)
-		existLabels = append(existLabels, label)
+	var addLabels []models.Labels
+	if len(report.Labels) == 0 {
+		_, err := o.QueryTable("Labels").All(&addLabels)
+		if err != nil {
+			utils.LogToFile("ERROR", fmt.Sprintf("report标签为空的时候获取可添加标签失败，错误信息: %s", err.Error()))
+		}
+	} else {
+		_, err = o.QueryTable("labels").Exclude("id__in", report.Labels).All(&addLabels)
+		if err != nil {
+			utils.LogToFile("ERROR", err.Error())
+		}
 	}
-	_, err = o.QueryTable("labels").All(&allLabels)
-	if err != nil {
-		utils.LogToFile("ERROR", fmt.Sprintf("查询所有标签报错，错误信息为: %s", err.Error()))
-	}
-
-	retireLabels := removeLabel(existLabels, allLabels)
-	r.Data["existLabels"] = existLabels
-	r.Data["labels"] = retireLabels
+	r.Data["existLabels"] = report.Labels
 	r.Data["report"] = report
+	r.Data["addLabels"] = addLabels
 	r.TplName = "sqlExe/m2m_add_label.html"
 }
 
@@ -74,39 +73,48 @@ func (r *ReportAddTagController) Post() {
 	id := r.GetString("id")
 	author := r.GetString("author")
 	report := models.ReportMany{}
-	parseInt, err := strconv.ParseInt(id, 10, 0)
-	if err != nil {
-		utils.LogToFile("ERROR", err.Error())
-	}
-	labelsId := r.GetStrings("labels")
-	report.Id = int(parseInt)
+	orm.Debug = true
 	o := orm.NewOrm()
-	qs := o.QueryTable("report_many").RelatedSel().Filter("id", id)
-	err = qs.One(&report)
+
+	err := o.QueryTable("report_many").Filter("id", id).One(&report)
 	if err != nil {
-		utils.LogToFile("ERROR", err.Error())
-	}
-	_, err = o.LoadRelated(&report, "Labels")
-	if err != nil {
-		utils.LogToFile("ERROR", fmt.Sprintf("查询report出错，错误信息: %v", err.Error()))
+		utils.LogToFile("ERROR", fmt.Sprintf("查询id为%d的report报错，错误信息为: %s", id, err.Error()))
 	}
 	report.Title = title
 	report.Description = description
 	report.Author = author
-	var label []models.Labels
-	var updateLabels []models.Labels
-	for i := range len(labelsId) - 1 {
-		idInt, _ := strconv.ParseInt(labelsId[i], 10, 0)
-		if err != nil {
-			utils.LogToFile("ERROR", err.Error())
-		} else {
-			o.QueryTable("Labels").Filter("id", idInt).One(&label)
-			updateLabels = append(updateLabels, label[i])
-		}
+	addLabels := r.GetStrings("addLabels")
+	existLabels := r.GetStrings("existLabels")
+	var modifiedLabels []models.Labels
+	var label = models.Labels{}
+	// 获取新增标签
+	for i := 0; i < len(addLabels); i++ {
+		labelId, _ := strconv.ParseInt(addLabels[i], 10, 64)
+		o.QueryTable("labels").Filter("id", labelId).One(&label)
+		modifiedLabels = append(modifiedLabels, label)
 	}
-	_, err = o.Update(report)
+	// 获取原有标签的修改
+	for i := 0; i < len(existLabels); i++ {
+		labelId, _ := strconv.ParseInt(existLabels[i], 10, 64)
+		o.QueryTable("labels").Filter("id", labelId).One(&label)
+		modifiedLabels = append(modifiedLabels, label)
+	}
+	var pModifyLabels []*models.Labels
+	for label := range modifiedLabels {
+		pModifyLabels = append(pModifyLabels, &modifiedLabels[label])
+	}
+	report.Labels = pModifyLabels
+	m2m := o.QueryM2M(&report, "Labels")
+	// 先删除再添加
+	_, err = m2m.Clear()
 	if err != nil {
-		utils.LogToFile("ERROR", fmt.Sprintf("report更新后结果插入数据库报错，错误信息为: %v", err.Error()))
+		utils.LogToFile("ERROR", fmt.Sprintf("id为%v的report多对多删除标签报错，错误信息为: %s", id, err.Error()))
+	}
+	for i := 0; i < len(modifiedLabels); i++ {
+		_, err := m2m.Add(modifiedLabels[i])
+		if err != nil {
+			utils.LogToFile("ERROR", fmt.Sprintf("id为%v的report多对多添加标签%v是报错，错误信息为: %s", id, label, err.Error()))
+		}
 	}
 	r.TplName = "ok.html"
 }
